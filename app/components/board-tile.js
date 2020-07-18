cde38a3efdad298e31c41aaac5ef9ae89c8bb344
Luka Salevic <salevic@lu-ka.me>
2020-07-18 16:36:08 +0200

    Clean up some of the board-tile logic regarding disabling user input

--- app/components/board-tile.js
+++ app/components/board-tile.js
@@ -19,10 +19,6 @@ export default class BoardTileComponent extends Component {
   @tracked winner = "";
 
   checkVictory(boardState) {
-    if (this.winner === "player" || this.winner === "enemy") {
-      this.setWinner();
-    }
-
     const board = chunk(boardState, 3);
     board.forEach(row => {
       let rowSum = 0;
@@ -40,44 +36,28 @@ export default class BoardTileComponent extends Component {
     })
   }
 
-  /**
-   * Upon reaching a victory or draw state, create an overlay over the game board with the outcome on it
-   */
-  createOverlay() {
+  setWinner() {
     const board = document.querySelector(".game-area");
     const status = document.querySelector(".status");
     const button = document.createElement("button");
 
+    board.style.background = "#FB9032";
+
+    setTimeout( () => {
     board.classList.remove("grid");
     board.classList.add("flex");
     button.innerHTML = "Play Again";
-    button.onclick = this.resetGame;
-
-    if (this.winner === "player") {
-      board.style.background = "#7CFFC4";
-      board.innerHTML = "You won!"
-    } else if ( this.winner === "enemy") {
-      board.style.background = "#FF837C"
-      board.innerHTML = "You lost!"
-    }
-
-    console.log(status.parentNode);
+    button.onclick = (() => { window.location.href = "/" });
     status.parentNode.replaceChild(button, status)
-  }
 
-  setWinner() {
-    const tiles = document.querySelectorAll(".grid-item");
-
-    tiles.forEach(tile => {
       if (this.winner === "player") {
-        tile.parentNode.removeChild(tile)
-      } else if (this.winner === "enemy") {
-        tile.classList.add("enemy-victory");
+        board.style.background = "#7CFFC4";
+        board.innerHTML = "You won!"
+      } else if ( this.winner === "enemy") {
+        board.style.background = "#FF837C"
+        board.innerHTML = "You lost!"
       }
-      tile.classList.remove("player-turn");
-    })
-
-    this.createOverlay()
+    }, 300)
   }
 
   /**
@@ -100,7 +80,6 @@ export default class BoardTileComponent extends Component {
         boardArray.push(0)
       }
     });
-    // window.localStorage.setItem("game-in-progress", boardArray);
     this.checkVictory(boardArray);
   }
 
@@ -152,7 +131,7 @@ export default class BoardTileComponent extends Component {
       this.playerTurn = true;
       this.checkGameState();
       this.toggleUserClick();
-    }, Math.random() * (800 - 500) + 500) // Simulate thinking with a small timeout
+    }, Math.random() * (400 - 200) + 200) // Simulate thinking with a small timeout
   }
 
   /**
@@ -183,6 +162,7 @@ export default class BoardTileComponent extends Component {
     if (tile.classList.contains("player-turn")) {
       if (tile.innerHTML === "X" || tile.innerHTML === "O") {
         alert("This tile is already occupied");
+        return // Don't play computerRound if user clicks on occupied block
       } else {
         tile.innerHTML = "X";
       }
@@ -191,14 +171,8 @@ export default class BoardTileComponent extends Component {
       if (this.winner === "") {
         this.computerRound();
       }
-    } else if (this.winner === "player" || this.winner === "enemy"){
+    } else if (this.winner === "player" || this.winner === "enemy") {
       return
     } else alert("It's not your turn!");
-  }
-
-  resetGame() {
-    debugger;
-    console.log("reset");
-    window.location.href = "/"
   }
 }
