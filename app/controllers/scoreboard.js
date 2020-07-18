6283d0372e1a15093eb198e26cb449eee26565e8
Luka Salevic <salevic@lu-ka.me>
2020-07-18 22:58:54 +0200

    Remove localScore model

--- app/controllers/scoreboard.js
+++ app/controllers/scoreboard.js
@@ -1,30 +1,4 @@
 import Controller from '@ember/controller';
-import {tracked} from "@glimmer/tracking";
-import { action } from '@ember/object';
 
 export default class ScoreboardController extends Controller {
-
-  @action
-  changeWin(amount) {
-    this.store.findRecord("local-score", 1).then((score) => {
-      score.wins = 2
-      score.save();
-    })
-  }
-
-  @action
-  changeDraw(amount) {
-    this.store.findRecord("local-score", 1).then((score) => {
-      score.draws += amount;
-      score.draws.save();
-    })
-  }
-
-  @action
-  changeLoss(amount) {
-    this.store.findRecord("local-score", 1).then((score) => {
-      score.losses += amount;
-      score.save();
-    })
-  }
 }
--- app/routes/scoreboard.js
+++ app/routes/scoreboard.js
@@ -3,7 +3,6 @@ import Route from '@ember/routing/route';
 export default class ScoreboardRoute extends Route {
   async model() {
     return {
-      localScore: this.store.findRecord('local-score', 1),
       score: this.store.findAll('score')
     }
   }
