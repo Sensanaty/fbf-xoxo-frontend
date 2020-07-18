33f64ddd9105d8313b494803042d0a6051d6baf6
Luka Salevic <salevic@lu-ka.me>
2020-07-18 23:29:04 +0200

    Display localStorage scores and get the global score store for scoreboard

--- app/routes/scoreboard.js
+++ app/routes/scoreboard.js
@@ -1,4 +1,15 @@
 import Route from '@ember/routing/route';
 
 export default class ScoreboardRoute extends Route {
+  model() {
+    return {
+      localScore: {
+        username: localStorage.getItem("username") || "Anonymous",
+        wins: localStorage.getItem("wins") || 0,
+        losses: localStorage.getItem("losses") || 0,
+        draws: localStorage.getItem("draws") || 0,
+      },
+      score: this.store.peekAll("score")
+    }
+  }
 }
