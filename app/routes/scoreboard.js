ac7d4e06019180c1454d8e71d5901a5c9724186a
Luka Salevic <salevic@lu-ka.me>
2020-07-18 23:16:05 +0200

    Make "score" model globally available through application router

--- app/routes/scoreboard.js
+++ app/routes/scoreboard.js
@@ -1,9 +1,4 @@
 import Route from '@ember/routing/route';
 
 export default class ScoreboardRoute extends Route {
-  async model() {
-    return {
-      score: this.store.findAll('score')
-    }
-  }
 }
