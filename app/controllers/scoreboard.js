52f1a18141d15dfa22b895c99d39673bd316c0fb
Luka Salevic <salevic@lu-ka.me>
2020-07-19 19:55:34 +0200

    Add a score reset button at the scoreboard template

--- app/controllers/scoreboard.js
+++ app/controllers/scoreboard.js
@@ -1,4 +1,14 @@
 import Controller from '@ember/controller';
+import {action} from "@ember/object";
 
 export default class ScoreboardController extends Controller {
+  @action resetScore() {
+    this.model.score.wins = 0;
+    this.model.score.draws = 0;
+    this.model.score.losses = 0;
+    localStorage.setItem("wins", 0);
+    localStorage.setItem("draws", 0);
+    localStorage.setItem("losses", 0);
+    window.location.reload(true);
+  }
 }
