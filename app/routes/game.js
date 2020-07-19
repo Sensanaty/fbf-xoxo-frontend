43e4f5ad0c1a815d52ec60082068fa25dcf689ea
Luka Salevic <salevic@lu-ka.me>
2020-07-19 18:47:50 +0200

    Adjust game and scoreboard routers

--- app/routes/game.js
+++ app/routes/game.js
@@ -3,7 +3,14 @@ import Route from '@ember/routing/route';
 export default class GameRoute extends Route {
     model() {
     return {
-      initialGameState: ["", "", "", "", "", "", "", "", ""]
+      initialGameState: ["", "", "", "", "", "", "", "", ""],
+      localScore: {
+        username: localStorage.getItem("username") || "Anonymous",
+        wins: localStorage.getItem("wins") || 0,
+        losses: localStorage.getItem("losses") || 0,
+        draws: localStorage.getItem("draws") || 0,
+      },
+      score: this.store.peekAll('score')
     }
   }
 }
--- app/routes/scoreboard.js
+++ app/routes/scoreboard.js
@@ -3,13 +3,13 @@ import Route from '@ember/routing/route';
 export default class ScoreboardRoute extends Route {
   model() {
     return {
+      score: this.store.peekAll("score"),
       localScore: {
         username: localStorage.getItem("username") || "Anonymous",
         wins: localStorage.getItem("wins") || 0,
         losses: localStorage.getItem("losses") || 0,
         draws: localStorage.getItem("draws") || 0,
-      },
-      score: this.store.peekAll("score")
+      }
     }
   }
 }
